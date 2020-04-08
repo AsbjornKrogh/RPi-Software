a63c4918bbb463e8e12e6b49d4c56717f8fa6891
Asbjørn Schöneberg Krogh <au651267@uni.au.dk>
2020-04-08 11:02:42 +0200

    Hello Pete

--- RaspberryPI Software/Program.cs
+++ RaspberryPI Software/Program.cs
@@ -6,13 +6,15 @@ using RaspberryPiCore.LCD;
 
 namespace Raspberry_Pi_Dot_Net_Core_Console_Application3
 {
+
+
    class Program
    {
+
+      //hello Pete 2
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
-
-         //Hello pete
+        
       }
    }
 }
